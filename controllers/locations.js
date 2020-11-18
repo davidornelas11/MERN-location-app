0b3697b68be37e0d9a94591b5ed8df6a3e5ac27b
david ornelas <davidornelas_11@hotmail.com>
2020-11-18 13:27:48 -0700

    created users controller

--- controllers/locations.js
+++ controllers/locations.js
@@ -48,39 +48,5 @@ locations.delete('/:id', (req, res) => {
 })
 
 
-///////////////////////////
-// create, post, delete and update user
-//////////////////////////
-
-//Create Route
-locations.post('/users', async (req, res) => {
-      User.create(req.body, (error, createdLocation) => {
-            if (error) {
-                  res.status(400).json({ error: error.message })
-            }
-            res.status(200).json(createdLocation)
-      })
-})
-
-//Update Route
-locations.put('/users/:id', (req, res) => {
-      User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedLocation) => {
-            if (err) {
-                  res.status(400).json({ error: err.message})
-            }
-            res.status(200).json(updatedLocation)
-      })
-})
-
-
-//Delete Route
-locations.delete('/users/:id', (req, res) => {
-      User.findByIdAndRemove(req.params.id, (err, deletedLocation) => {
-            if (err)
-            res.status(400).json({error: err.message})
-      })
-      res.status(200).json(deletedLocation)
-})
-
 
 module.exports = locations
